14e3bd2af54394bda1dce88a4f578d141b3e571c
wlwl0219 <wlwl0219@gmail.com>
2021-01-07 15:26:08 +0900

    Update gcd solution

--- urclass/practice/gcd.js
+++ urclass/practice/gcd.js
@@ -2,8 +2,9 @@
 // Examples
 // gcd(22, 24);  2
 
+// solution 1
+// 두 수의 공통된 약수중 최대수인 수를 반환
 function gcd(num1, num2) {
-  // 두 수의 공통된 약수중 최대수인 수를 반환
   let arr1 = [];
   for (let i = 1; i <= num1; i++) {
     if (num1 % i === 0) {
@@ -15,5 +16,18 @@ function gcd(num1, num2) {
   return Math.max(...arr1);
 }
 
-// test start
-// 1. 항상 배열을 리턴해야합니다.
+// solution 2
+// 유클리드 호제법을 이용
+// 정확히 나누어 진다면 두번째 수 반환
+// 나누어 질때 까지 두번째 수와 나머지를 나눈다.
+function gcd(num1, num2) {
+  return num1 % num2 === 0 ? num2 : gcd(num2, num1 % num2);
+}
+
+// solution 3
+// 유클리드 호제법을 이용
+// 두번째 수가 0이라면 첫번째 수 반환
+// 나누어 질때 까지 두번째 수와 나머지를 나눈다.
+function gcd(num1, num2) {
+  return num2 ? gcd(num2, num1 % num2) : num1;
+}
