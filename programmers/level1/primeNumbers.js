282ffc92c7aeedf8f60fb3d414b6e29be7c2d24d
wlwl0219 <wlwl0219@gmail.com>
2021-01-28 17:30:46 +0900

    Add primeNumber problem

---
+++ programmers/level1/primeNumbers.js
@@ -0,0 +1,12 @@
+// 1부터 입력받은 숫자 n 사이에 있는
+// 소수의 개수를 반환하는 함수, solution을 만들어 보세요.
+
+// Precautions
+// n은 2이상 1000000이하의 자연수입니다.
+
+// Examples
+// 10 > 4
+// 5 > 3
+
+// solution 1
+function solution(n) {}
