bb78342c54927492a052c2354315d641c16a3a3a
wlwl0219 <wlwl0219@gmail.com>
2021-01-31 03:35:52 +0900

    Add primeNumber solution

--- programmers/level1/primeNumbers.js
+++ programmers/level1/primeNumbers.js
@@ -9,4 +9,19 @@
 // 5 > 3
 
 // solution 1
-function solution(n) {}
+// 주어진 수만 큼의 길이를 가지고 모든 값은 true를 가진 배열을 만든다.
+// 첫번째는 false를 할당한다.
+// 2부터 n의 제곱근까지 반복하여 만난 수의 배수들을 false로 만든다.
+// 최종으로 만들어진 배열의 요소중 true를 값으로 가진 요소만 개수를 새어 반환한다.
+function solution(n) {
+  const arr = new Array(n).fill(true);
+  arr[0] = false;
+  for (let i = 2; i ** 2 <= n; i++) {
+    if (arr[i - 1] === true) {
+      for (let j = i * 2; j <= n; j += i) {
+        arr[j - 1] = false;
+      }
+    }
+  }
+  return arr.filter((e) => e).length;
+}
