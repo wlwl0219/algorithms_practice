5a4b9b367166a72b5b92c3586894d4f8532c06bf
wlwl0219 <wlwl0219@gmail.com>
2021-03-16 00:28:16 +0900

    Add problem, solution

---
+++ programmers/level1/dollDrawGame.js
@@ -0,0 +1,52 @@
+// 게임 화면의 격자의 상태가 담긴 2차원 배열 board와
+// 인형을 집기 위해 크레인을 작동시킨 위치가 담긴 배열 moves가
+// 매개변수로 주어질 때, 크레인을 모두 작동시킨 후 터트려져
+// (인형이 없는 곳에서 크레인을 작동시키는 경우에는 아무런 일도 일어나지 않습니다)
+// (같은 모양의 인형이 모인 갯수 만큼 터트려짐)
+// 사라진 인형의 개수를 return 하도록 solution 함수를 완성해주세요.
+
+// Precautions
+// board 배열은 2차원 배열로 크기는 "5 x 5" 이상 "30 x 30" 이하입니다.
+// board의 각 칸에는 0 이상 100 이하인 정수가 담겨있습니다.
+// 0은 빈 칸을 나타냅니다.
+// 1 ~ 100의 각 숫자는 각기 다른 인형의 모양을 의미하며
+// 같은 숫자는 같은 모양의 인형을 나타냅니다.
+// moves 배열의 크기는 1 이상 1,000 이하입니다.
+// moves 배열 각 원소들의 값은 1 이상이며
+// board 배열의 가로 크기 이하인 자연수입니다.
+
+// solution 1
+// moves배열에 들어있는 원소(인형의 위치) 하나씩 뽑아
+// board의 그 자리가 0으로 바뀌고 그 인형을 반환하는 함수를 실행한다.
+// 그 함수의 실행결과를 임의의 배열(dollList)에 담는다.
+// 뽑힌 인형을 담은 배열을 순회하며 같은 수가 연속으로 있는 개수를 세어 반환한다.
+// -> 시간초과
+function solution(board, moves) {
+  function pickDoll(board, num) {
+    for (let i = 0; i < board.length; i++) {
+      if (board[i][num - 1]) {
+        let doll = board[i][num - 1];
+        board[i][num - 1] = 0;
+        return doll;
+      }
+    }
+  }
+
+  let dollArray = [];
+  for (let i = 0; i < moves.length; i++) {
+    dollArray.push(pickDoll(board, moves[i]));
+  }
+  let dollList = dollArray.filter(ele => ele);
+
+  let i = 0;
+  let count = 0;
+  while (i < dollList.length) {
+    if (dollList[i] === dollList[i + 1]) {
+      count = count + 2;
+      dollList.splice(i, 2);
+      i = i - 3;
+    }
+    i++;
+  }
+  return count;
+}
