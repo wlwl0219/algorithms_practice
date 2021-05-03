8cd77edd89716f0988bf194a3c87f8029c2292b8
wlwl0219 <wlwl0219@gmail.com>
2021-05-03 22:34:44 +0900

    Add fibonacci, rockPaperScissors problems

---
+++ urclass/advance/solution/rockPaperScissors.js
@@ -0,0 +1,20 @@
+// 가위바위보 게임의 수를 나타내는
+// 양의 정수 rounds가 주어질 경우,
+// 해당 rounds 동안 선택할 수 있는
+// 모든 경우의 수를 리턴하도록 함수를 작성해 보세요.
+
+// output
+// 2차원 배열을 리턴해야 합니다.
+// arr[i].length는 3
+// arr[i]는 'rock', 'paper', 'scissors'
+// 중 한 가지 이상을 요소로 갖는 배열입니다
+
+// Precautions
+// 최종적으로 리턴되는 배열의 순서는
+// 가중치 적용 정렬(Weighted Sort)을 따릅니다.
+// 중요도는 'rock', 'paper', 'scissors' 순으로 높습니다.
+// 쉽게 생각해 올림픽 순위 결정 방식을 참고하면 됩니다.
+// 금메달('rock')이 은메달('paper')보다 우선하고,
+// 은메달('paper')이 동메달('scissors')보다 우선합니다.
+
+const rockPaperScissors = function (rounds) {};
